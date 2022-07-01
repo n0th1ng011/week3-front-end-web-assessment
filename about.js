76204bb1562f975f08ae0d16d677aea582659d35
Po <po@Pos-MacBook-Pro.localdomain>
2022-07-01 11:58:02 -0600

    Initial Commit

---
+++ about.js
@@ -0,0 +1,24 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('Form successfuly submitted');
+}
+
+
+let form = document.querySelector('#contact');
+
+form.addEventListener('submit', handleSubmit);
+
+let image = document.querySelector('#img src')
+
+function reveal(evt) {
+	evt.preventDefault();
+
+	alert('Congrats, you have found and captured Dio!')
+}
+
+image.addEventListener('mouseover', reveal)
